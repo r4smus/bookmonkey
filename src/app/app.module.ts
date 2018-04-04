5554aedefe31655a1f5243eaf694cf317095b7b9
DESKTOP-1PF6GOH\chris <christian.kienle85@gmail.com>
2018-04-04 13:22:37 +0200

    it2-di (dependeny injection/services)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 
+import { BookStoreService } from './shared/book-store.service';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,7 @@ import { BookDetailsComponent } from './book-details/book-details.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [BookStoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
